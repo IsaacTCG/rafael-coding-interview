a9bbcc2166ea72ed49f4e9c48714022eeac30bb5
Isaac Tavares Caldas Gonçalves <isaaccaldasgoncalves@gmail.com>
2025-03-12 14:19:01 -0300

    [REFACT] Refactore tweets_controller to be more simple and add scope in tweet model

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -2,17 +2,18 @@ class TweetsController < ApplicationController
   
   def index
     per_page = 5
-    next_index = Tweet.last.id
+    current_index = Tweet.last.id
 
     per_page = search_params[:per_page]&.to_i if search_params[:per_page].present?
-    next_index = search_params[:next_index]&.to_i if search_params[:next_index].present?
+    current_index = search_params[:next_index]&.to_i if search_params[:next_index].present?
 
-    next_last_tweet_index_by_per_page = next_index - (per_page - 1)
+    last_index_to_get = current_index - (per_page - 1)
 
-    tweets = Tweet.order(created_at: :desc)
-    tweets = tweets.where("id >= ?", next_last_tweet_index_by_per_page) if next_index.present? && next_index.positive?
+    tweets = Tweet
+                .newest
+                .by_index(last_index_to_get, current_index)
     
-    next_index = next_last_tweet_index_by_per_page - 1
+    next_index = last_index_to_get - 1
     next_index = nil if next_index < 1
 
     render json: {
--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,2 +1,5 @@
 class Tweet < ApplicationRecord
+  scope :newest, -> () { order(created_at: :desc) }
+  scope :oldest, -> () { order(created_at: :asc) }
+  scope :by_index, -> (last_index_to_get, current_index) { where("id >= ?", last_index_to_get) if current_index.present? && current_index.positive? }
 end
