df9b4b7a414431ccc486da789d5664e0f9b61ad0
Isaac Tavares Caldas Gonçalves <isaaccaldasgoncalves@gmail.com>
2025-03-12 12:13:25 -0300

    [TESTS] Add more tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,49 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  RSpec.shared_context 'with multiple companies' do
+    let!(:company_1) { create(:company) }
+    let!(:company_2) { create(:company) }
+
+    before do
+      5.times { create(:user, company: company_1) }
+      5.times { create(:user, company: company_2) }
+    end
+
+    let!(:users) { User.all }
+  end
+
+  describe "scopes" do
+    include_context 'with multiple companies'
+
+    context ".by_company" do
+      it "returns users belonging to a specific company" do
+        expect(User.by_company(company_1.id).pluck(:id)).to match_array(company_1.users.pluck(:id))
+        expect(User.by_company(company_2.id).pluck(:id)).to match_array(company_2.users.pluck(:id))
+      end
+    end
+
+    context ".by_username" do
+      it "returns users whose display_name contains the search term" do
+        search_name = company_1.users.first.display_name
+        expect(User.by_username(search_name).pluck(:id)).to match_array(company_1.users.where("LOWER(display_name) LIKE LOWER(?)", "%#{search_name}%").pluck(:id))
+      end
+
+      it "is case-insensitive" do
+        search_name = company_1.users.first.display_name.downcase
+        expect(User.by_username(search_name).pluck(:id)).to match_array(company_1.users.where("LOWER(display_name) LIKE LOWER(?)", "%#{search_name}%").pluck(:id))
+      end
+
+      it "returns an empty result when no match is found" do
+        expect(User.by_username("NonexistentUser")).to be_empty
+      end
+    end
+
+    context "combining scopes" do
+      it "returns users filtered by company and username" do
+        search_name = company_1.users.first.display_name
+        expect(User.by_company(company_1.id).by_username(search_name).pluck(:id)).to match_array(company_1.users.where("LOWER(display_name) LIKE LOWER(?)", "%#{search_name}%").pluck(:id))
+      end
+    end
+  end
 end
--- spec/requests/user_request_spec.rb
+++ spec/requests/user_request_spec.rb
@@ -7,12 +7,8 @@ RSpec.describe "Users", type: :request do
     let!(:company_2) { create(:company) }
 
     before do
-      5.times do
-        create(:user, company: company_1)
-      end
-      5.times do
-        create(:user, company: company_2)
-      end
+      5.times { create(:user, company: company_1) }
+      5.times { create(:user, company: company_2) }
     end
 
     let!(:users) { User.all }
@@ -31,6 +27,13 @@ RSpec.describe "Users", type: :request do
         expect(result.map { |element| element['id'] }).to eq(company_1.users.ids)
       end
 
+      it "returns an empty result when no match for company_id is found" do
+        get company_users_path('INVALID_IDENTIFIER')
+        
+        expect(result).to be_empty
+        expect(result.size).to eq(0)
+      end
+
       it 'returns all the users with specified username' do
         get company_users_path(company_1), params: { username: company_1.users.first.display_name }
 
@@ -41,8 +44,8 @@ RSpec.describe "Users", type: :request do
       it 'returns all the users with partial username' do
         get company_users_path(company_2), params: { username: company_2.users.first.display_name[0..2] }
 
-        expect(result.size).to eq(company_2.users.where('display_name LIKE ?', "%#{company_2.users.first.display_name[0..2]}%").count)
-        expect(result.map { |element| element['id'] }).to match_array(company_2.users.where('display_name LIKE ?', "%#{company_2.users.first.display_name[0..2]}%").pluck(:id)) 
+        expect(result.size).to eq(company_2.users.where('LOWER(display_name) LIKE LOWER(?)', "%#{company_2.users.first.display_name[0..2]}%").count)
+        expect(result.map { |element| element['id'] }).to match_array(company_2.users.where('LOWER(display_name) LIKE LOWER(?)', "%#{company_2.users.first.display_name[0..2]}%").pluck(:id)) 
       end
     end
 
@@ -66,8 +69,15 @@ RSpec.describe "Users", type: :request do
       it 'returns all the users with partial username' do
         get users_path, params: { username: company_2.users.first.display_name[0..2] }
 
-        expect(result.size).to eq(users.where('display_name LIKE ?', "%#{company_2.users.first.display_name[0..2]}%").count)
-        expect(result.map { |element| element['id'] }).to match_array(users.where('display_name LIKE ?', "%#{company_2.users.first.display_name[0..2]}%").pluck(:id)) 
+        expect(result.size).to eq(users.where('LOWER(display_name) LIKE LOWER(?)', "%#{company_2.users.first.display_name[0..2]}%").count)
+        expect(result.map { |element| element['id'] }).to match_array(users.where('LOWER(display_name) LIKE LOWER(?)', "%#{company_2.users.first.display_name[0..2]}%").pluck(:id)) 
+      end
+
+      it "returns an empty result when no match is found" do
+        get users_path, params: { username: 'NonexistentUser' }
+
+        expect(result).to be_empty
+        expect(result.size).to eq(0)
       end
     end
   end
